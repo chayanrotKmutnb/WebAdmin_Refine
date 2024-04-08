d6d2ed8a52ec5d1eecb01eb3390d041c6f833033
ChayanrotJA <s6303051613084@email.kmutnb.ac.th>
2024-04-08 12:29:18 +0700

    Update edit.tsx

--- src/pages/user/edit.tsx
+++ src/pages/user/edit.tsx
@@ -1,10 +1,9 @@
 import {
   FormControl,
-  FormErrorIcon,
-  FormErrorMessage,
   FormLabel,
   Input,
   Select,
+  FormErrorMessage,
 } from "@chakra-ui/react";
 import { Edit } from "@refinedev/chakra-ui";
 import { useForm } from "@refinedev/react-hook-form";
@@ -12,17 +11,16 @@ import { useForm } from "@refinedev/react-hook-form";
 export const PostEdit = () => {
   const { register, handleSubmit, formState: { errors } } = useForm<IUser>();
 
-  const onSubmit = (data: IUser) => console.log(data);
+  const onSubmit = data => console.log(data);
 
   return (
     <Edit>
-      {/* ใช้ `handleSubmit` กับ `form` ใน `Edit` component */}
       <form onSubmit={handleSubmit(onSubmit)}>
         <FormControl isInvalid={!!errors.firstName}>
           <FormLabel htmlFor="firstName">First Name</FormLabel>
           <Input id="firstName" type="text" {...register("firstName")} />
           <FormErrorMessage>
-            {errors.firstName?.message}
+            {errors.firstName && errors.firstName.message}
           </FormErrorMessage>
         </FormControl>
 
@@ -30,18 +28,20 @@ export const PostEdit = () => {
           <FormLabel htmlFor="lastName">Last Name</FormLabel>
           <Input id="lastName" type="text" {...register("lastName")} />
           <FormErrorMessage>
-            {errors.lastName?.message}
+            {errors.lastName && errors.lastName.message}
           </FormErrorMessage>
         </FormControl>
 
+        {/* เพิ่มฟิลด์ Nick Name */}
         <FormControl isInvalid={!!errors.nickName}>
           <FormLabel htmlFor="nickName">Nick Name</FormLabel>
           <Input id="nickName" type="text" {...register("nickName")} />
           <FormErrorMessage>
-            {errors.nickName?.message}
+            {errors.nickName && errors.nickName.message}
           </FormErrorMessage>
         </FormControl>
 
+        {/* เพิ่มฟิลด์ Gender */}
         <FormControl isInvalid={!!errors.gender}>
           <FormLabel htmlFor="gender">Gender</FormLabel>
           <Select id="gender" {...register("gender")}>
@@ -50,25 +50,28 @@ export const PostEdit = () => {
             <option value="other">อื่นๆ</option>
           </Select>
           <FormErrorMessage>
-            {errors.gender?.message}
+            {errors.gender && errors.gender.message}
           </FormErrorMessage>
         </FormControl>
 
+        {/* เพิ่มฟิลด์ Contact Number */}
         <FormControl isInvalid={!!errors.contactNumber}>
           <FormLabel htmlFor="contactNumber">Contact Number</FormLabel>
           <Input id="contactNumber" type="text" {...register("contactNumber")} />
           <FormErrorMessage>
-            {errors.contactNumber?.message}
+            {errors.contactNumber && errors.contactNumber.message}
           </FormErrorMessage>
         </FormControl>
 
+        {/* เพิ่มฟิลด์ Profile Image URL */}
         <FormControl isInvalid={!!errors.profileImageUrl}>
           <FormLabel htmlFor="profileImageUrl">Profile Image URL</FormLabel>
           <Input id="profileImageUrl" type="text" {...register("profileImageUrl")} />
-          <FormErrorIcon>
-            {errors.profileImageUrl?.message}
-            </FormErrorIcon>
+          <FormErrorMessage>
+            {errors.profileImageUrl && errors.profileImageUrl.message}
+          </FormErrorMessage>
         </FormControl>
+
       </form>
     </Edit>
   );
